2fd70e04d79a7c0c92ca9e796aaee04eb8a5f9f0
AlbertoF17 <albertofergom17@gmail.com>
2022-11-29 19:43:24 +0100

    Java

---
+++ Practicas/CaracteresEspeciales.java
@@ -0,0 +1,12 @@
+package Practicas;
+
+public class CaracteresEspeciales {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hola\tMundo");
+        System.out.println("\nHola    Mundo");
+        System.out.print(" \"'comillas dobles'\" " + "a\\a");
+        Thread.sleep(2000);
+        System.out.println("\rRetorno de carro :D");
+        System.out.println("Este retorno de carro no funciona porque no tiene nada a su derecha\r");
+    }
+}
