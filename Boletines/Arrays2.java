0133ab3dc1010d08d3f161f8e4bfcd620cd12db6
AlbertoF17 <albertofergom17@gmail.com>
2023-01-25 17:05:05 +0100

    Java

--- Boletines/Arrays2.java
+++ Boletines/Arrays2.java
@@ -197,8 +197,7 @@ public class Arrays2 {
         System.out.println("------------------------");
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                if ((j == 0 && i == array.length-1) || (i == array.length/2 && j == array.length/2)
-                        || (j == array.length-1 && i == 0)){
+                if (j == (array.length-1)-i){
                     array[i][j] = 1;
                     System.out.print(array[i][j] + " ");
                 } else {
@@ -238,14 +237,17 @@ public class Arrays2 {
         //2. Modificar el ejercicio anterior para que cada posición tenga un número aleatorio
         //entre 0 y 9.
         System.out.println("EJERCICIO 2");
-        int dimensionAleatoria = rand.nextInt(0, 10);
-        for (int i = 0; i < dimensionAleatoria; i++) {
-            for (int j = 0; j < dimensionAleatoria; j++) {
-                System.out.print("1 ");
+        System.out.print("Introduzca la dimensión de la matriz: ");
+        int dimension2 = sc.nextInt();
+        int[][] array2 = new int[dimension2][dimension2];
+        for (int i = 0; i < dimension2; i++) {
+            for (int j = 0; j < dimension2; j++) {
+                array2[i][j] = rand.nextInt(0, 10);
+                System.out.print(array2[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("Dimensión: " + dimensionAleatoria);
+        System.out.println("Dimensión: " + dimension2);
         System.out.println();
 
         //3. Modificar el ejercicio anterior para que las dimensiones sean nxm.
@@ -254,9 +256,11 @@ public class Arrays2 {
         int filas = sc.nextInt();
         System.out.print("Introduzca las columnas de la matriz: ");
         int columnas = sc.nextInt();
+        int[][] array3 = new int[filas][columnas];
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                System.out.print("1 ");
+                array3[i][j] = rand.nextInt(0, 10);
+                System.out.print(array3[i][j] + " ");
             }
             System.out.println();
         }
@@ -270,17 +274,17 @@ public class Arrays2 {
         //a 0 0 0
         System.out.println("EJERCICIO 4");
         System.out.print("Introduzca las dimension de la matriz: ");
-        int dimension1 = sc.nextInt();
-        String caracter;
+        int dimension4 = sc.nextInt();
+        String caracter4;
         do{
             System.out.print("Introduzca un caracter: ");
-            caracter = sc.next();
-        }while(caracter.length()!=1);
+            caracter4 = sc.next();
+        }while(caracter4.length()!=1);
 
-        for (int i = 0; i < dimension1; i++) {
-            for (int j = 0; j < dimension1; j++) {
+        for (int i = 0; i < dimension4; i++) {
+            for (int j = 0; j < dimension4; j++) {
                 if(i == 0 || j == 0){
-                    System.out.print(caracter);
+                    System.out.print(caracter4 + " ");
                 }else {
                     System.out.print("0 ");
                 }
@@ -296,21 +300,79 @@ public class Arrays2 {
         //0 0 0 a
         //0 0 0 a
         //a a a a
+        System.out.println("EJERCICIO 5");
+        System.out.print("Introduzca las dimension de la matriz: ");
+        int dimension5 = sc.nextInt();
+        String caracter5;
+        do{
+            System.out.print("Introduzca un caracter: ");
+            caracter5 = sc.next();
+        }while(caracter5.length()!=1);
+
+        for (int i = 0; i < dimension5; i++) {
+            for (int j = 0; j < dimension5; j++) {
+                if(i == dimension5-1 || j == dimension5-1){
+                    System.out.print(caracter5 + " ");
+                }else {
+                    System.out.print("0 ");
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
         //6. Leer por pantalla un carácter y un número n, e imprimir por pantalla una matriz
         //cuadrada como la siguiente. Ejemplo para ‘a’ y 4:
         //a a a a
         //a 0 0 a
         //a 0 0 a
         //a a a a
+        System.out.println("EJERCICIO 6");
+        System.out.print("Introduzca las dimension de la matriz: ");
+        int dimension6 = sc.nextInt();
+        String caracter6;
+        do{
+            System.out.print("Introduzca un caracter: ");
+            caracter6 = sc.next();
+        }while(caracter6.length()!=1);
+
+        for (int i = 0; i < dimension6; i++) {
+            for (int j = 0; j < dimension6; j++) {
+                if(i == dimension6-1 || j == dimension6-1 || i == 0 || j == 0){
+                    System.out.print(caracter5 + " ");
+                }else {
+                    System.out.print("0 ");
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
         //7. Leer por pantalla un número entero n, y crear una matriz de dimensiones nxn como
         //la que se muestra a continuación. Imprimir la matriz por pantalla.
         //Ejemplo para n = 4:
         //0 1 0 1
         //1 0 1 0
-        //
         //0 1 0 1
         //1 0 1 0
-        //
+        System.out.println("EJERCICIO 6");
+        System.out.print("Introduzca las dimension de la matriz: ");
+        int dimension7 = sc.nextInt();
+        for (int i = 0; i < dimension7; i++) {
+            for (int j = 0; j < dimension7; j++) {
+                if((i+j)%2!= 0){
+                    System.out.print("1 ");
+                }else {
+                    System.out.print("0 ");
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
         //8. (MUY DIFICIL) Modificar el ejercicio 2 para que una vez generada la matriz, se cree
         //otra nueva que contenga en cada posición la media aritmética de los elementos que
         //la rodean. Por ejemplo, para la siguiente matriz aleatoria:
@@ -321,6 +383,54 @@ public class Arrays2 {
         //1.67 1.2 2.0
         //4.0 3.125 2.4
         //2.33 2.4 2.67
-
+        System.out.println("EJERCICIO 8");
+        System.out.print("Introduzca la dimensión de la matriz: ");
+        int dimension8 = sc.nextInt();
+        int[][] array8A = new int [dimension8][dimension8];
+        for (int i = 0; i < dimension8; i++) {
+            for (int j = 0; j < dimension8; j++) {
+                array8A[i][j] = rand.nextInt(0, 10);
+                System.out.print(array8A[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nNUEVO ARRAY\n");
+        float[][] array8B = new float[dimension8][dimension8];
+        for (int i = 0; i < dimension8; i++) {
+            for (int j = 0; j < dimension8; j++) {
+                if(j-1<0) {
+                    if (i-1<0){
+                        array8B[i][j] = (array8A[i+1][j] + array8A[i][j+1] + array8A[i+1][j+1])/3f;
+                    }else if (i+1>=array8A.length){
+                        array8B[i][j] = (array8A[i][j+1] + array8A[i-1][j] + array8A[i-1][j+1])/3f;
+                    }else {
+                        array8B[i][j] = (array8A[i+1][j] + array8A[i][j+1] + array8A[i+1][j+1] + array8A[i-1][j+1]
+                                + array8A[i-1][j])/5f;
+                    }
+                }else if (j+1>=array8A.length){
+                    if (i-1<0){
+                        array8B[i][j] = (array8A[i][j-1] + array8A[i+1][j] + array8A[i+1][j-1])/3f;
+                    }else if (i+1>=array8A.length){
+                        array8B[i][j] = (array8A[i-1][j-1] + array8A[i-1][j] + array8A[i][j-1])/3f;
+                    }else {
+                        array8B[i][j] = (array8A[i-1][j] + array8A[i+1][j] + array8A[i+1][j-1] + array8A[i][j-1]
+                                + array8A[i-1][j-1])/5f;
+                    }
+                }else {
+                    if (i-1<0){
+                        array8B[i][j] = (array8A[i][j+1] + array8A[i+1][j] + array8A[i+1][j-1] + array8A[i+1][j+1]
+                                + array8A[i][j-1])/5f;
+                    } else if (i+1>=array8A.length) {
+                        array8B[i][j] = (array8A[i][j+1] + array8A[i-1][j] + array8A[i-1][j-1] + array8A[i-1][j+1]
+                                + array8A[i][j-1])/5f;
+                    }else {
+                        array8B[i][j] = (array8A[i-1][j-1] + array8A[i+1][j+1] + array8A[i][j+1] + array8A[i+1][j]
+                                + array8A[i][j-1] + array8A[i-1][j] + array8A[i-1][j+1] + array8A[i+1][j-1])/8f;
+                    }
+                }
+                System.out.printf("%1.2f ", array8B[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
