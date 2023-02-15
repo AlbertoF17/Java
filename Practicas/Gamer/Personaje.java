af89e8d0bab16390f849ea49a2acc88afdbb1958
AlbertoF17 <albertofergom17@gmail.com>
2023-02-15 16:01:22 +0100

    Java

--- Practicas/Gamer/Personaje.java
+++ Practicas/Gamer/Personaje.java
@@ -1,11 +1,22 @@
 package Practicas.Gamer;
 
+import java.util.Random;
+
 public class Personaje {
+    static Random rand = new Random();
     static String nombre;
     static int edad;
     static int altura;
     static int saludMaxima;
     static int saludActual;
+    //atributos SPECIAL
+    static int strength = rand.nextInt(1, 11);
+    static int perception = rand.nextInt(1, 11);
+    static int endurance = rand.nextInt(1, 11);
+    static int charisma = rand.nextInt(1, 11);
+    static int intelligence = rand.nextInt(1, 11);
+    static int agility = rand.nextInt(1, 11);
+    static int luck = rand.nextInt(1, 11);
 
     public static void recibirDanyo(int damage){
         saludActual = saludActual-damage;
