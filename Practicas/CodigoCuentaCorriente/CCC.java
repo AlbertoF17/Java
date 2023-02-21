692ba842f8a75cf7720aff0d8ee8d6c70593eb1f
AlbertoF17 <albertofergom17@gmail.com>
2023-02-21 20:48:22 +0100

    Java

--- Practicas/CodigoCuentaCorriente/CCC.java
+++ Practicas/CodigoCuentaCorriente/CCC.java
@@ -14,24 +14,50 @@ public class CCC {
         this.numeroDeCuenta = rand.nextLong();
     }
 
-    public double setIngresos(double cantidadIngresada){
-        return saldoDeCuenta+=cantidadIngresada;
+    public void setIngresos(double cantidadIngresada){
+        if (cantidadIngresada>0) {
+            saldoDeCuenta += cantidadIngresada;
+        } else {
+            throw new IllegalArgumentException("No se puede ingresar una cantidad igual o inferior a 0  ");
+        }
     }
 
     public double setReintegros(double cantidadReintegrada){
         return saldoDeCuenta-=cantidadReintegrada;
     }
 
+    public void setSaldoDeCuenta(double saldoDeCuenta){
+        if (saldoDeCuenta >= 0){
+            this.saldoDeCuenta = saldoDeCuenta;
+        } else {
+            throw new IllegalArgumentException("No se pueden tener números negativos en la cuenta");
+        }
+    }
+
     public double getSaldoDeCuenta() {
         return saldoDeCuenta;
     }
 
-    public String getDatosCuenta() {
-        return nombreDelTitular + ": " + numeroDeCuenta;
+    public String[] imprimirArray(String[] array){
+        for (int i = 0; i < array.length-1; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println(array[array.length-1]);
+        return array;
+    }
+
+    public String[] getDatosCuenta() {
+        String[] array = {String.valueOf(saldoDeCuenta), nombreDelTitular, String.valueOf(numeroDeCuenta)};
+        return imprimirArray(array);
     }
 
     public static void realizarTransferenciaEntreCuentas(CCC cuenta1, CCC cuenta2, double cantidad){
         cuenta1.saldoDeCuenta -= cantidad;
         cuenta2.saldoDeCuenta += cantidad;
+    }
+    //@Override anotación no necesaria que indica que está "sobreescribiendo" un método ya creado
+    public String toString(){
+        return ("CCC{Nº de cuenta: " + numeroDeCuenta + ", titular de la cuenta: " + nombreDelTitular + ", saldo de" +
+                " la cuenta: " + saldoDeCuenta + "}");
     }
 }
--- Practicas/CodigoCuentaCorriente/OperacionesBanco.java
+++ Practicas/CodigoCuentaCorriente/OperacionesBanco.java
@@ -6,7 +6,8 @@ public class OperacionesBanco {
         CCC CCC_2 = new CCC("Antonio", 400);
         System.out.println(CCC_1.getDatosCuenta());
         System.out.println(CCC_2.getDatosCuenta());
-        System.out.println(CCC_1.setIngresos(100));
+        CCC_1.setIngresos(100);
+        System.out.println(CCC_1.getSaldoDeCuenta());
         System.out.println(CCC_2.setReintegros(100));
         System.out.println(CCC_1.getSaldoDeCuenta());
         CCC.realizarTransferenciaEntreCuentas(CCC_1, CCC_2, 50);
