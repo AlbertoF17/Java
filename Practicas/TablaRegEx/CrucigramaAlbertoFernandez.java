973f2d79468cf0d2092c6f35fb3659ad51d82242
AlbertoF17 <albertofergom@gmail.com>
2023-03-23 00:42:12 +0100

    Java

--- Practicas/TablaRegEx/CrucigramaAlbertoFernandez.java
+++ Practicas/TablaRegEx/CrucigramaAlbertoFernandez.java
@@ -8,14 +8,72 @@ public class CrucigramaAlbertoFernandez {
     String[] col;
     String[] row;
     char[][] sol;
-
+    int maxLen;
+    public CrucigramaAlbertoFernandez() {
+        this.col = new String[]{"."};
+        this.row = new String[]{"."};
+        this.sol = new char[1][1];
+        this.maxLen = 1;
+    }
     public CrucigramaAlbertoFernandez(String[] col, String[] row) {
         this.col = col;
         this.row = row;
         sol = new char[row.length][col.length];
+        this.maxLen = 1;
     }
 
-    public void introducirSolucion(){
+    public void setSol(char[][] sol) {
+        this.sol = sol;
+    }
+    private void validarRow(String[] row){
+        for (int i = 0; i < row.length; i++) {
+            if (row[i].length() != 1){
+                throw new IllegalArgumentException("La fila debe tener un sólo carácter en cada espacio que ocupa");
+            }
+        }
+        if(row == null){
+            throw new NullPointerException("El array no puede ser nulo");
+        }
+    }
+    private void validarCol(String[] col){
+        for (int i = 0; i < col.length; i++) {
+            if (col[i].length() != 1){
+                throw new IllegalArgumentException("La columna debe tener un sólo carácter en cada espacio que ocupa");
+            }
+        }
+        if(col == null){
+            throw new NullPointerException("El array no puede ser nulo");
+        }
+    }
+    private void validarSol(char[][] sol){
+        for (int i = 0; i < sol.length; i++) {
+            for (int j = 0; j < sol[i].length; j++) {
+                if ((sol[i][j]+"").length() != 1){
+                    throw new IllegalArgumentException("La solución debe tener un sólo carácter en cada espacio" +
+                            " de la matriz");
+                }
+            }
+        }
+        if(sol == null){
+            throw new NullPointerException("La matriz no puede ser nula");
+        }
+    }
+    public int maxLong(){
+        int maxLong = 0;
+        for (int i = 0; i < col.length; i++) {
+            if (col[i].length() > maxLong){
+                maxLong = col[i].length();
+            }
+            if (row[i].length() > maxLong){
+                maxLong = row[i].length();
+            }
+        }
+        return maxLong;
+    }
+    public String ampliar(String str){
+        return " ".repeat(maxLen-str.length()) + str + " |";
+    }
+    public void introducirSol(){
         int numCelda = 1;
         for (int i = 0; i < sol.length; i++) {
             for (int j = 0; j < sol[i].length; j++) {
@@ -26,7 +84,7 @@ public class CrucigramaAlbertoFernandez {
         }
     }
 
-    public void comprobarSolucion(){
+    public boolean comprobarSol(){
         boolean solucionCorrecta = true;
         for (int i = 0; i < sol.length; i++) {
             for (int j = 0; j < sol[i].length; j++) {
@@ -40,6 +98,7 @@ public class CrucigramaAlbertoFernandez {
         }else{
             System.out.println("Fallaste, inténtalo de nuevo");
         }
+        return solucionCorrecta;
     }
 
     public void borrarSol(){
