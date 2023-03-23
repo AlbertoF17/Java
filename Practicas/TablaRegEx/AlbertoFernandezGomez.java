d6fc58eebbd031af48751a77f2f6f4feb31a2e88
AlbertoF17 <albertofergom17@gmail.com>
2023-03-23 19:07:00 +0100

    Java

--- Practicas/TablaRegEx/AlbertoFernandezGomez.java
+++ Practicas/TablaRegEx/AlbertoFernandezGomez.java
@@ -5,9 +5,19 @@ public class AlbertoFernandezGomez {
         CrucigramaAlbertoFernandez crucigrama1 = new CrucigramaAlbertoFernandez(
                 new String[]{"[JUNDT]*","APA|OPI|OLK","(NA|FE|HE)[CV]" },
                 new String[]{"[DEF][MNO]*","[^DJNU]P[ABC]","[ICAN]*"});
-        System.out.println(crucigrama1);
-        crucigrama1.introducirSol();
-        System.out.println(crucigrama1);
-        crucigrama1.comprobarSol();
+        CrucigramaAlbertoFernandez crucigrama2 = new CrucigramaAlbertoFernandez(
+                new String[]{"UB|IE|AW","[TUBE]*","[BORF]." },
+                new String[]{"[NOTAD]*","WEL|BAL|EAR"});
+        CrucigramaAlbertoFernandez crucigrama3 = new CrucigramaAlbertoFernandez(
+                new String[]{"[BQW](PR|LE)","[RANK]+"},
+                new String[]{"[AWE]+","[ALP]+K","(PR|ER|EP)"});
+        CrucigramaAlbertoFernandez[] arrayCrucicramas =
+                new CrucigramaAlbertoFernandez[]{crucigrama1,crucigrama2,crucigrama3};
+        for (int i = 0; i < arrayCrucicramas.length; i++) {
+            System.out.println(arrayCrucicramas[i]);
+            arrayCrucicramas[i].introducirSol();
+            System.out.println(arrayCrucicramas[i]);
+            arrayCrucicramas[i].comprobarSol();
+        }
     }
 }
--- Practicas/TablaRegEx/CrucigramasOpcAlbertoFernandez.java
+++ Practicas/TablaRegEx/CrucigramasOpcAlbertoFernandez.java
@@ -1,4 +1,5 @@
 package Practicas.TablaRegEx;
 
 public class CrucigramasOpcAlbertoFernandez {
+
 }
