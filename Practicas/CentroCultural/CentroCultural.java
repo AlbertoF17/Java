1c4e221bd87e2ec183e871b84e7d629f1d1e8865
AlbertoF17 <albertofergom17@gmail.com>
2023-05-12 16:58:18 +0200

    Java

--- Practicas/CentroCultural/CentroCultural.java
+++ Practicas/CentroCultural/CentroCultural.java
@@ -1,10 +1,9 @@
 package Practicas.CentroCultural;
 
 public interface CentroCultural {
-    int maxLibros = 15;
-    int maxDiscos = 15;
-    int maxClientes = 5;
-    int maxPrestamos = 6;
+
+    int maxLibros = 3;
+    int maxDiscos = 3;
 
     void registrarCliente(Cliente cliente) throws RegistroClienteException;
     void registrarMaterial(Material material) throws RegistroMaterialException;
